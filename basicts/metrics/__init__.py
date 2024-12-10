600da13e5dc935d4f0b15ef41d64711b3c326ffd
lx <arthurlx94@163.com>
2024-12-10 14:12:49 +0800

    'update'

--- basicts/metrics/__init__.py
+++ basicts/metrics/__init__.py
@@ -3,9 +3,6 @@ from .mape import masked_mape
 from .mse import masked_mse
 from .rmse import masked_rmse
 from .wape import masked_wape
-from .smape import masked_smape
-from .r_square import masked_r2
-from .corr import masked_corr
 
 ALL_METRICS = {
             'MAE': masked_mae,
@@ -13,9 +10,6 @@ ALL_METRICS = {
             'RMSE': masked_rmse,
             'MAPE': masked_mape,
             'WAPE': masked_wape,
-            'SMAPE': masked_smape,
-            'R2': masked_r2,
-            'CORR': masked_corr
             }
 
 __all__ = [
@@ -24,8 +18,5 @@ __all__ = [
     'masked_rmse',
     'masked_mape',
     'masked_wape',
-    'masked_smape',
-    'masked_r2',
-    'masked_corr',
     'ALL_METRICS'
 ]
